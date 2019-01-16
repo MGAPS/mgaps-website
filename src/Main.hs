272344f1bdb32da6d33a502ab4c1f353fa1f6ef3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-01-16 16:24:44 -0500

    Changed aknowledgements

--- src/Main.hs
+++ src/Main.hs
@@ -286,5 +286,5 @@ aknowledgement = unlines [
     , "* Icons are provided by the [Font Awesome project](https://fontawesome.com/)"
     , "* Website compiler is built on top of the [Hakyll library](http://hackage.haskell.org/package/hakyll)"
     , ""
-    , "Source code to the website compiler is available on [GitHub](https://github.com/LaurentRDC/mgaps-website)."
+    , "Source code and content is available on [GitHub](https://github.com/LaurentRDC/mgaps-website)."
     ]
