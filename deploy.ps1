c2b7c89966f9daf69e1f33255a1605246341a078
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-01-16 12:06:06 -0500

    Added verbose mode to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -4,6 +4,6 @@
 $SSH_DEPLOY_KEY >> 'id_rsa'
 '-----END RSA PRIVATE KEY-----' >> 'id_rsa'
 
-scp -B -r -i ./id_rsa _rendered $REMOTE_DEPLOY_LOCATION
+scp -B -v -r -i ./id_rsa _rendered $REMOTE_DEPLOY_LOCATION
 
 Remote-Item 'id_rsa'
